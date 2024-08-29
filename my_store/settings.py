d0c9c7e122cbb05f1f4c802d6475d358df38ce8d
Alexander Hebbes <aeh7@live.co.uk>
2024-08-29 22:54:23 +0000

    Remove secret key from settings

--- my_store/settings.py
+++ my_store/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p=-1tkfk@2q+#7y7+@fjan$e&cj2idf$(@*g5e-(l7s_na1h_l'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

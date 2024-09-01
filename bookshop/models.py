5744556bcf058b398296fa59a307a63a227bb47d
Alexander Hebbes <aeh7@live.co.uk>
2024-09-01 00:40:36 +0000

    Create profile model for user information

--- bookshop/models.py
+++ bookshop/models.py
@@ -24,3 +24,16 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    address_line1 = models.CharField(max_length=255)
+    address_line2 = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100)
+    county = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"

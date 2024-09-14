6734e7354190c285e2757866711c3f13d414a0a2
Alexander Hebbes <aeh7@live.co.uk>
2024-09-14 02:10:47 +0000

    Update urls and begin books.html

--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+def all_books(request):
+    """ view to show all books including sortiing and search queries """
+
+    books = Book.objects.all()
+
+    context = {
+        'products' : products,
+    }
+
+
+
+    return render(request, 'bookshop/books.html', context)

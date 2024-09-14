44348cfd0ffd109a973ff9ce50953bb2649218d6
Alexander Hebbes <aeh7@live.co.uk>
2024-09-14 00:52:20 +0000

    Change location of Book model

---
+++ books/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+BOOK_CATEGORIES = [
+    ('classics', 'Classics'),
+    ('historical', 'Historical'),
+    ('mystery_thriller', 'Mystery & Thriller'),
+    ('scifi_fantasy', 'Science Fiction & Fantasy'),
+    ('biography', 'Biography & Memoir'),
+    ('other', 'Other'),
+]
+
+class Book(models.Model):
+    book_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True, help_text="13 Character ISBN number")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100, choices=BOOK_CATEGORIES, default='other')
+    description = models.TextField(blank=True, null=True)
+    image_path = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- bookshop/models.py
+++ bookshop/models.py
@@ -3,27 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-BOOK_CATEGORIES = [
-    ('classics', 'Classics'),
-    ('historical', 'Historical'),
-    ('mystery_thriller', 'Mystery & Thriller'),
-    ('scifi_fantasy', 'Science Fiction & Fantasy'),
-    ('biography', 'Biography & Memoir'),
-    ('other', 'Other'),
-]
-
-class Book(models.Model):
-    book_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    author = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=100, choices=BOOK_CATEGORIES, default='other')
-    description = models.TextField(blank=True, null=True)
-    image_path = models.CharField(max_length=255, blank=True, null=True)
-
-    def __str__(self):
-        return self.title
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)

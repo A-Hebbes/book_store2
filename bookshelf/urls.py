02e074bad68f4bd07180048c1d26cada27b2df01
Alexander Hebbes <aeh7@live.co.uk>
2024-09-22 22:44:32 +0000

    Add remove from bookshelf button and update urls and views for bookshelf

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_bookshelf, name='view_bookshelf'),
     path('add/<book_id>/', views.add_to_bookshelf, name='add_to_bookshelf'),
+    path('remove/<book_id>/', views.remove_from_bookshelf, name='remove_from_bookshelf'),
 ]
--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.urls import reverse
+from books.models import Book
 
 def view_bookshelf(request):
     """ View to render bookshelf page """
+    bookshelf = request.session.get('bookshelf', {})
+    bookshelf_items = []
 
-    return render(request, 'bookshelf/bookshelf.html')
+    for book_id, quantity in bookshelf.items():
+        book = get_object_or_404(Book, book_id=int(book_id))
+        bookshelf_items.append({
+            'book': book,
+            'quantity': quantity,
+        })
 
+    context = {
+        'bookshelf_items': bookshelf_items,
+    }
+
+    return render(request, 'bookshelf/bookshelf.html', context)
 
 def add_to_bookshelf(request, book_id):
     """ Add a quantity of the book to the bookshelf """
@@ -13,10 +27,21 @@ def add_to_bookshelf(request, book_id):
     redirect_url = request.POST.get('redirect_url')
     bookshelf = request.session.get('bookshelf', {})
 
-    if book_id in list(bookshelf.keys()):
-        bookshelf[book_id] += quantity
+    if str(book_id) in list(bookshelf.keys()):
+        bookshelf[str(book_id)] += quantity
     else:
-        bookshelf[book_id] = quantity
+        bookshelf[str(book_id)] = quantity
 
     request.session['bookshelf'] = bookshelf
     return redirect(redirect_url)
+
+def remove_from_bookshelf(request, book_id):
+    """Remove the book from the bookshelf"""
+    bookshelf = request.session.get('bookshelf', {})
+
+    if str(book_id) in bookshelf:
+        del bookshelf[str(book_id)]
+        request.session['bookshelf'] = bookshelf
+        messages.success(request, "Removed book from your bookshelf")
+
+    return redirect(reverse('view_bookshelf'))

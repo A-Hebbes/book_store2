0069740455394477b368bcef88b0b328c054faa3
Alexander Hebbes <aeh7@live.co.uk>
2024-09-22 00:25:48 +0000

    Uncomment form for adding to bookshelf and aligned urls

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bookshelf, name='view_bookshelf')
+    path('', views.view_bookshelf, name='view_bookshelf'),
+    path('add/<book_id>/', views.add_to_bookshelf, name='add_to_bookshelf'),
 ]
--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bookshelf(request):
     """ View to render bookshelf page """
 
     return render(request, 'bookshelf/bookshelf.html')
+
+
+def add_to_bookshelf(request, book_id):
+    """ Add a quantity of the book to the bookshelf """
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bookshelf = request.session.get('bookshelf', {})
+
+    if book_id in list(bookshelf.keys()):
+        bookshelf[book_id] += quantity
+    else:
+        bookshelf[book_id] = quantity
+
+    request.session['bookshelf'] = bookshelf
+    print(request.session['bookshelf'])
+    return redirect(redirect_url)

3fb585912559347b74c8ce2341e8684d7dd75a84
Alexander Hebbes <aeh7@live.co.uk>
2024-09-29 23:13:55 +0000

    Add signals.py to checkout

---
+++ checkout/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Updates total on lineitem when changed or cretaed
+    """
+    instance.order.update_total()
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Updates total when item is delted
+    """
+    instance.order.update_total()

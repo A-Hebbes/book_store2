944ebfa372545da9e8a030dbf6c578993c54f23a
Alexander Hebbes <aeh7@live.co.uk>
2024-09-30 22:39:08 +0000

    Add views and urls for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+from bookshelf.contexts import bookshelf_contents
+
+def checkout(request):
+    bookshelf = request.session.get('bookshelf', {})
+    if not bookshelf:
+        messages.error(request, "Your bookshelf is empty at the moment")
+        return redirect(reverse('books'))
+    
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)

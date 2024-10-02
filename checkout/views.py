7409d3a6acd3e401dfcabd399a5598be766b84fb
Alexander Hebbes <aeh7@live.co.uk>
2024-10-02 09:00:57 +0000

    Update checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -8,6 +8,9 @@ from bookshelf.contexts import bookshelf_contents
 import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bookshelf = request.session.get('bookshelf', {})
     if not bookshelf:
         messages.error(request, "Your bookshelf is empty at the moment")
@@ -16,12 +19,23 @@ def checkout(request):
     current_bookshelf = bookshelf_contents(request)
     total = current_bookshelf['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+        )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing.')
+
+    
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51Q5EeK00Qa65K0ZpaITJ0e4Dmm7zZa9GXfMVQgwiQKI4uKW8PQFRrAF3Wyby00qxcpknP0crUHkqA0Nb3kFTRDyd00mdiV69NA'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)

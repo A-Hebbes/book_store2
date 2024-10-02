97937ce8449a1039aed513f276940a2e6c9844dc
Alexander Hebbes <aeh7@live.co.uk>
2024-10-02 22:36:08 +0000

    Add checkout cache data function to views.py

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,23 @@ from books.models import Book
 from bookshelf.contexts import bookshelf_contents
 
 import stripe
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bookshelf': json.dumps(request.session.get('bookshelf', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user.username if request.user.is_authenticated else 'AnonymousUser',
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=str(e), status=400)
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -74,24 +91,28 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        if request.user.is_authenticated:
-            try:
-                profile = UserProfile.objects.get(user=request.user)
-                order_form = OrderForm(initial={
-                    'full_name': profile.user.get_full_name(),
-                    'email': profile.user.email,
-                    'phone_number': profile.default_phone_number,
-                    'country': profile.default_country,
-                    'postal_code': profile.default_postal_code,
-                    'city': profile.default_city,
-                    'address_line1': profile.default_address_line1,
-                    'address_line2': profile.default_address_line2,
-                    'county': profile.default_county,
-                })
-            except UserProfile.DoesNotExist:
-                order_form = OrderForm()
-        else:
-            order_form = OrderForm()
+#        if request.user.is_authenticated:
+#            try:
+#                profile = UserProfile.objects.get(user=request.user)
+#                order_form = OrderForm(initial={
+#                    'full_name': profile.user.get_full_name(),
+#                    'email': profile.user.email,
+#                    'phone_number': profile.default_phone_number,
+#                    'country': profile.default_country,
+#                    'postal_code': profile.default_postal_code,
+#                    'city': profile.default_city,
+#                    'address_line1': profile.default_address_line1,
+#                    'address_line2': profile.default_address_line2,
+#                    'county': profile.default_county,
+#                })
+#            except UserProfile.DoesNotExist:
+#                order_form = OrderForm()
+#        else:
+#            order_form = OrderForm()
+# Instead always create new orderform
+
+    order_form = OrderForm()
+
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing.')
@@ -114,26 +135,27 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user)
-        # Attach user profile to order
-        order.user_profile = profile
-        order.save()
+     # Comment out UserProfile related code
+    # if request.user.is_authenticated:
+    #     profile = UserProfile.objects.get(user=request.user)
+    #     # Attach user profile to order
+    #     order.user_profile = profile
+    #     order.save()
 
-        # Save the user info
-        if save_info:
-            profile_data = {
-                'default_phone_number': order.phone_number,
-                'default_country': order.country,
-                'default_postal_code': order.postal_code,
-                'default_city': order.city,
-                'default_address_line1': order.address_line1,
-                'default_address_line2': order.address_line2,
-                'default_county': order.county,
-            }
-            user_profile_form = UserProfileForm(profile_data, instance=profile)
-            if user_profile_form.is_valid():
-                user_profile_form.save()
+    #     # Save the user info
+    #     if save_info:
+    #         profile_data = {
+    #             'default_phone_number': order.phone_number,
+    #             'default_country': order.country,
+    #             'default_postal_code': order.postal_code,
+    #             'default_city': order.city,
+    #             'default_address_line1': order.address_line1,
+    #             'default_address_line2': order.address_line2,
+    #             'default_county': order.county,
+    #         }
+    #         user_profile_form = UserProfileForm(profile_data, instance=profile)
+    #         if user_profile_form.is_valid():
+    #             user_profile_form.save()
 
     messages.success(request, f'Order successfully processed \
         Order number {order_number}. Confirmation \

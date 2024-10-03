3885a1c79338d1382e64fe87123bbd3259338fdf
Alexander Hebbes <aeh7@live.co.uk>
2024-10-03 09:20:44 +0000

    Update debug in settings.py

--- my_store/settings.py
+++ my_store/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = [
     '8000-ahebbes-bookstore2-jvgphak8du4.ws.codeinstitute-ide.net',

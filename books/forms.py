8f97a3d63dcb5f2bb419120a11007de03e7e6f53
Alexander Hebbes <aeh7@live.co.uk>
2024-10-05 05:41:19 +0000

    Update BookForm in books forms.py

--- books/forms.py
+++ books/forms.py
@@ -1,7 +1,39 @@
 from django import forms
 from .models import Book, BOOK_CATEGORIES
 
+class BookForm(forms.ModelForm):
+    image = forms.ImageField(label='Image', required=False)
+    
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'isbn', 'price', 'category', 'description']
+    
+    category = forms.ChoiceField(choices=BOOK_CATEGORIES)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+
+    def clean_isbn(self):
+        isbn = self.cleaned_data.get('isbn')
+        if isbn is not None and len(isbn) != 13:
+            raise forms.ValidationError("ISBN must be 13 characters long.")
+        return isbn
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if self.cleaned_data.get('image'):
+            # Handle the image file and set image_path
+            # This part depends on how you want to store and name your files
+            instance.image_path = f"books/{self.cleaned_data['image'].name}"
+        if commit:
+            instance.save()
+        return instance
+
+
+"""
+OLD BOOK FORM COMMENTED TO TEST FOR IMAGE UPLOAD
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
@@ -20,11 +52,6 @@ class BookForm(forms.ModelForm):
             raise forms.ValidationError("ISBN must be 13 characters long.")
         return isbn
 """
-    def clean_isbn(self):
-        isbn = self.cleaned_data.get('isbn')
-        if len(isbn) != 13:
-            raise forms.ValidationError("ISBN must be 13 characters long.")
-        return isbn
-"""
+
 
 

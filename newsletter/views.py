77c72fff9d8d5b047b0b71253c984208bb7d129c
Alexander Hebbes <aeh7@live.co.uk>
2024-10-06 04:56:30 +0000

    Add views.py to newsletter

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NewsletterForm
 
 # Create your views here.
+
+def newsletter_signup(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'You have successfully subscribed')
+            return redirect(reverse('all_books'))  
+    else:
+        form = NewsletterForm()
+        messages.error(request, 'Something went wrong. Check the form and resubmit')
+        return render(request, 'newsletter/signup.html', {'form': form})

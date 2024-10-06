01971b71d2366088781b00c3385ffc775261ca34
Alexander Hebbes <aeh7@live.co.uk>
2024-10-06 08:14:16 +0000

    Add conatct html and views.

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
+from .forms import ContactForm
 
 # Create your views here.
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent. Thank you')
+            return redirect(reverse('all_books'))
+        else:
+            messages.error(request, 'Something went wrong. Please check the form and resubmit.')
+    else:
+        form = ConatctForm()
+    
+    return render(request, 'contact/contact.html', {'form': form})

736045969e187f229c067ca1c3f86861e3f945d3
Alexander Hebbes <aeh7@live.co.uk>
2024-10-16 05:34:14 +0000

    Add faq urls.py and update main urls.py

--- my_store/urls.py
+++ my_store/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('newsletter/', include('newsletter.urls')),
     path('contact/', include('contact.urls')),
+    path('faq/', include('faq.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

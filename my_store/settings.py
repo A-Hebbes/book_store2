09968474c34d47c2a614a9ce621a02442eb83377
Alexander Hebbes <aeh7@live.co.uk>
2024-10-19 09:14:21 +0000

    Update settings for AWS

--- my_store/settings.py
+++ my_store/settings.py
@@ -201,7 +201,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    # Bucket Config
+    # Bucket Config 
     AWS_STORAGE_BUCKET_NAME = 'bookshop2-09bd4357cc3b'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')

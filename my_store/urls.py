7482040074510d3d5522bcd8ea1ab9a43a9b5eba
Alexander Hebbes <aeh7@live.co.uk>
2024-10-19 08:25:09 +0000

    Add 404 page

--- my_store/urls.py
+++ my_store/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 
 urlpatterns = [
@@ -31,3 +32,4 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('faq/', include('faq.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'my_store.views.handler404'

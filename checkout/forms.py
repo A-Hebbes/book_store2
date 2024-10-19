b9899267599af9af990a8f9fa67507abb050681b
Alexander Hebbes <aeh7@live.co.uk>
2024-10-19 05:31:07 +0000

    Add background to checkout success page

--- checkout/forms.py
+++ checkout/forms.py
@@ -24,15 +24,15 @@ class OrderForm(forms.ModelForm):
             'address_line1': 'Address Line 1',
             'address_line2': 'Address Line 2',
             'county': 'County, State or Locality',
+            'country': 'Country',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if field != 'country':
                 if self.fields[field].required:
                     placeholder = f'{placeholders[field]} *'
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].label = False
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+                self.fields[field].label = False

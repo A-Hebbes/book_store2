419c89eed2f7ffff1ac63bc35b2b85fa2719cf94
Alexander Hebbes <aeh7@live.co.uk>
2024-10-20 22:55:55 +0000

    Validate and format bookshop python files

--- bookshop/models.py
+++ bookshop/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                primary_key=True)
     address_line1 = models.CharField(max_length=255)
     address_line2 = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=100)
--- bookshop/views.py
+++ bookshop/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render
 
+
 def index(request):
     """ view to return the index page """
     return render(request, 'bookshop/index.html')
-
-"""
-def my_shop(request):
-    
-    return render(request, 'bookshop/my_shop.html')
-
-def books(request):
-
-    return render(request, 'bookshop/books.html')
-"""
